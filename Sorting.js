787cdd83c1787204660adb3b0c6b02a1c31d6bf8
ATrillet <andrew.trillet@gmail.com>
2019-12-01 11:44:24 -0500

    test

--- Sorting.js
+++ Sorting.js
@@ -1,8 +1,21 @@
+let toSort = [];
+let columns = 100;
 function setup() {
-
+  createCanvas(windowWidth, windowHeight);
+  for (let nums = 0 ; nums < columns ; nums++) {
+    toSort.push(random(0,100));
+  }
+  print(toSort);
 }
 
 
 function draw() {
-
+  background(0);
+  stroke('white');
+  for (let i = 0 ; i < toSort.length ; i++) {
+    rect(i*width/columns, height, width/columns - 5, -toSort[i]*7.16);
+  }
+  
+  
+  
 }
