f2fd14903430d85369eea3f0390c956f9c679462
ATrillet <andrew.trillet@gmail.com>
2019-12-04 21:25:19 -0500

    implemented bubble sort and will draw the process

--- Sorting.js
+++ Sorting.js
@@ -1,22 +1,51 @@
 let toSort = [];
-let columns = 100;
+let columns = 5;
+let bubblei = 0;
+let bubblej = 0;
+let col = 'white';
+let fil = 'white';
+let factor;
+let max = 100;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   for (let nums = 0 ; nums < columns ; nums++) {
-    toSort.push(random(0,100));
+    toSort.push(random(0,max));
   }
-  print(toSort);
-  //branch tests
+  factor = height/max;
+}
+
+function bubbleSort(arr, i, j) {
+   n = arr.length;
+   if (i < n) {
+     if (j < n-i-1) {
+       if (arr[j] > arr[j+1]) {
+         let temp = arr[j];
+         arr[j] = arr[j+1];
+         arr[j+1] = temp;
+       }
+     } else {
+       bubblei++;
+       bubblej=0;
+     }
+   } else {
+    col='green'; 
+    fil = 'green';
+   }
+   //bubblej++;
+   return arr;
 }
 
 
 function draw() {
   background(0);
-  stroke('white');
+  stroke(col);
+  fill(fil);
   for (let i = 0 ; i < toSort.length ; i++) {
-    rect(i*width/columns, height, width/columns - 5, -toSort[i]*7.16);
+    rect(i*width/columns, height, width/columns - 5, -toSort[i]*factor);
   }
-  
-  
-  
+  print(bubblei);
+  print(bubblej);
+  bubbleSort(toSort, bubblei, bubblej++);
+  stroke(col);
 }
